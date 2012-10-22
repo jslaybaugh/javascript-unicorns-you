fc18e04baf050d76651fc83a08d705220d9d9000
Jorin Slaybaugh <jslaybaugh@gmail.com>
2012-10-22 16:55:10 -0500

    all there

---
+++ examples/knockout.js
@@ -0,0 +1,105 @@
+﻿(function ()
+{
+	var me;
+
+	var clearForm = function()
+	{
+		me.FirstName("");
+		me.LastName("");
+		me.Phone("");
+		me.Error("");
+	};
+
+	this.ViewModel = Class.extend({
+		init: function ()
+		{
+			me = this;
+
+			me.ErrorPresent = ko.computed(function() { return me.Error().length > 0; });
+		},
+
+		FirstName: ko.observable(""),
+		LastName: ko.observable(""),
+		Phone: ko.observable(""),
+		Error: ko.observable(""),
+		ErrorPresent: undefined,
+		People: ko.observableArray(),
+
+		AddPerson: function()
+		{
+			me.People.push(new Person(me.FirstName(), me.LastName(), me.Phone()));
+			clearForm();
+		},
+
+		RemovePerson: function(data)
+		{
+			if (confirm("are you sure?"))
+			me.People.remove(data);
+		}
+
+	});
+
+}).call(this);
+
+(function ()
+{
+	var me;
+	var phoneRegex = /^(\+[1]|[1])?(?:[\(-. /]*)(\d{3})(?:[\)-. /]*)(\d{3})(?:[-. /]*)(\d{4})(?:\s?[A-Za-z]*[,.]?\s?)(\d*)$/;
+	var formatPhone = function (number)
+	{
+		if (number == null) return null;
+
+		number = unformatPhone(number);
+
+		if (number != null)
+		{
+			if (number.match(phoneRegex))
+			{
+				if ($.trim(number.replace(phoneRegex, "$5")).length > 0)
+				{
+					number = number = $.trim(number.replace(phoneRegex, "$1 ($2) $3-$4 ext. $5"))
+				}
+				else
+				{
+					number = number = $.trim(number.replace(phoneRegex, "$1 ($2) $3-$4"))
+				}
+			}
+		}
+		return number;
+	};
+
+	var unformatPhone = function (number)
+	{
+		if ($.trim(number.replace(phoneRegex, "$5")).length > 0)
+			number = $.trim(number.replace(phoneRegex, "$1$2$3$4,$5"));
+		else
+			number = $.trim(number.replace(phoneRegex, "$1$2$3$4"));
+
+		return number;
+	};
+
+	this.Person = Class.extend({
+		init: function (firstName, lastName, phone)
+		{
+			me = this;
+			me.FirstName(firstName);
+			me.LastName(lastName);
+			me.Phone(unformatPhone(phone));
+			me.FormattedPhone = ko.computed(function() { return formatPhone(me.Phone()); })
+			me.FullName = ko.computed(function() { return me.LastName() + ", " + me.FirstName(); });
+		},
+
+		FirstName: ko.observable(""),
+		LastName: ko.observable(""),
+		Phone: ko.observable(""),
+		FormattedPhone: undefined,
+		FullName: undefined,
+
+		Call: function()
+		{
+			alert("Now Calling " + me.Phone());
+		}
+
+	});
+
+}).call(this);
--- nodetask/app.js
+++ nodetask/app.js
@@ -4,45 +4,62 @@ var http = require('http')
 var server = http.createServer(app)
 var io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen(80);
 
 // routing
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+	res.sendfile(__dirname + '/index.html');
 });
 
-// usernames which are currently connected to the chat
+// usernames which are currently connected to the task group
 var usernames = {};
+// task history
+var tasks = [];
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket)
+{
+	// when a client connects, update to show them all tasks since restart
+	io.sockets.emit('updatetasks', tasks);
 
-  // when the client emits 'sendchat', this listens and executes
-  socket.on('addtask', function (data) {
-    // we tell the client to execute 'updatechat' with 2 parameters
-    io.sockets.emit('addtask', socket.username, data);
-  });
+	// when the client emits 'addtask', this listens and executes
+	socket.on('addtask', function (data)
+	{
+ 		tasks.push(data);
+ 		data.Id = tasks.length - 1;
+ 		io.sockets.emit('addtask', socket.username, data);
+ 		if (data.Task.toUpperCase().indexOf("UNICORN") >= 0)
+ 		{
+ 			io.sockets.emit("cornify");
+ 		}
+	});
 
-  // when the client emits 'adduser', this listens and executes
-  socket.on('adduser', function(username){
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    // echo to client they've connected
-    //socket.emit('updatechat', 'SERVER', 'you have connected');
-    // echo globally (all clients) that a person has connected
-   // socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-    // update the list of users in chat, client-side
-    io.sockets.emit('updateusers', usernames);
-  });
+	// when the client emits 'taskcompleted', this listens and executes
+	socket.on("taskcompleted", function (index, completed)
+	{
+ 		tasks[index].Completed = completed;
+ 		io.sockets.emit("taskcompleted", index, completed);
+	});
 
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function(){
-    // remove the username from global usernames list
-    delete usernames[socket.username];
-    // update list of users in chat, client-side
-    io.sockets.emit('updateusers', usernames);
-    // echo globally that this client has left
-   // socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-  });
+	// when the client emits 'removetask', this listens and executes
+	socket.on('removetask', function (index)
+	{
+ 		tasks.splice(index, 1);
+ 		io.sockets.emit('removetask', index);
+	});
+
+	// when the client emits 'adduser', this listens and executes
+	socket.on('adduser', function (username)
+	{
+ 		socket.username = username;
+ 		usernames[username] = username;
+ 		io.sockets.emit('updateusers', usernames);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', function ()
+	{
+ 		// remove the username from global usernames list
+ 		delete usernames[socket.username];
+ 		io.sockets.emit('updateusers', usernames);
+	});
 });
